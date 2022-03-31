79f0f90469f6443981da7ffd567951a014d6fea9
Gleibson Pereira <gleibson.santos05@gmail.com>
2022-03-30 23:17:11 -0300

    proximo comit

--- admin_page.php
+++ admin_page.php
@@ -4,10 +4,10 @@
 
 if(isset($_POST['add_product'])){
 
-    $product_name = $_POST['add_name'];
-    $product_price = $_POST['add_price'];
-    $product_image = $_POST['add_image']['name'];
-    $product_image_tmp_name = $_POST['add_image']['tmp_name'];
+    $product_name = $_POST['product_name'];
+    $product_price = $_POST['product_price'];
+    $product_image = $_FILES['product_image']['name'];
+    $product_image_tmp_name = $_FILES['product_image']['tmp_name'];
     $product_image_folder = 'uploaded_img/'.$product_image;
 
     if(empty($product_name) || empty($product_price) || empty($product_image)){
