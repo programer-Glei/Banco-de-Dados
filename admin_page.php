563c65555b8a96e32f745c3674971ff5447293b3
Gleibson Pereira <gleibson.santos05@gmail.com>
2022-04-01 22:41:13 -0300

    proximo comit

--- admin_page.php
+++ admin_page.php
@@ -51,7 +51,7 @@ if(isset($_POST['add_product'])){
                 <input type="text" placeholder="Digite o nome do produto" name="product_name" class="box">
                 <input type="number" placeholder="Digite o preço" name="product_price" class="box">
                 <input type="file" acept="image/png, image/jpeg, image/jpg" name="product_image" class="box">
-                <input type="submit" class="novo" name="add_product" value="adicionar produto">
+                <input type="submit" class="btn" name="add_product" value="adicionar produto">
             </form>
         </div>
     </div>
